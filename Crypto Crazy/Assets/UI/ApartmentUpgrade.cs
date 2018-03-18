8a2db0b9c3eccc9d43cab412ec9b3e82fac13910
Kirill M <kmuravskiy@gmail.com>
2018-03-18 14:22:23 -0400

    Started building the apartment upgrade system and its UI.

---
+++ Crypto Crazy/Assets/UI/ApartmentUpgrade.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ApartmentUpgrade : MonoBehaviour {
+
+    public Image icon;
+    public Text title;
+    public Text descText;
+    public Text miscText;
+    public Button buyButton;
+    public Image darkOverlay;
+    public GameObject alreadyOwnedText;
+
+    public Apartment myApartment;
+
+    [SerializeField]
+    private bool active;
+    private LevelUpgrader levelUpgrader;
+
+	// Use this for initialization
+	void Start () {
+        levelUpgrader = FindObjectOfType<LevelUpgrader>();
+
+    }
+
+    private void OnEnable()
+    {
+        // Activating the UI elements on this apartment 
+        if (active)
+        {
+            darkOverlay.gameObject.SetActive(false);
+            alreadyOwnedText.gameObject.SetActive(false);
+
+            icon.sprite = myApartment.myIcon;
+            title.text = myApartment.myTitle;
+            descText.text = myApartment.myDescText;
+
+
+        }
+    }
+
+    void AssignFunctionToButton()
+    {
+        // Here we decide what the BUY APARTMENT button does
+        buyButton.onClick.AddListener(() => levelUpgrader.UpgradeToDifferentLvl(myApartment.myPrefab));
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+
+
+	}
+}
--- Crypto Crazy/Assets/_Levels/Apartment.cs
+++ Crypto Crazy/Assets/_Levels/Apartment.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class Apartment : ScriptableObject {
 
-	
+    public string myTitle;
+    public Sprite myIcon;
+    [TextArea(3,10)]
+    public string myDescText;
+    public int rackSlots;
+  
+
+    public GameObject myPrefab;
 }
